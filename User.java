8d83df35873afcf9b8ca020e0c0134dc8a20d5fc
Amrutha Holla <152254465+ammuholla@users.noreply.github.com>
2026-02-16 21:58:30 +0530

    Add User class with userId and name properties

---
+++ User.java
@@ -0,0 +1,22 @@
+public class User {
+    private int userId;
+    private String name;
+
+    public User(int userId, String name) {
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void display() {
+        System.out.println("User ID: " + userId +
+                ", Name: " + name);
+    }
+}
