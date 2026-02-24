59fe6a7beeb29bbb76f01e0411ad0413358ce477
Amrutha Holla <152254465+ammuholla@users.noreply.github.com>
2026-02-24 20:14:40 +0530

    Create employee_db and employees table

---
+++ Employee.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE employees (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    salary DOUBLE,
+    department VARCHAR(100)
+);
